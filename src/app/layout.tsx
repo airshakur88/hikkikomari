c51e1ee24ea1c01aa79c5357a291bab69aa95fd6
ZyRox <airshakur88@gmail.com>
2026-05-28 14:56:47 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,14 +19,14 @@ export const metadata: Metadata = {
     images: [
       {
         url: "https://hikkikomari.lol/21951af0-b994-47a1-beb6-bf9f2194c3a8.jpg",
-        width: 1200,
-        height: 630,
+        width: 512,
+        height: 512,
         alt: "~ Hikkikomari ~",
       },
     ],
   },
   twitter: {
-    card: "summary_large_image",
+    card: "summary",
     title: "~ Hikkikomari ~",
     images: [
       "https://hikkikomari.lol/21951af0-b994-47a1-beb6-bf9f2194c3a8.jpg",
